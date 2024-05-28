82c0af816a309908ca94f0c4aff4d27285aa73d1
Alexandr <sasha_stepanov94@mail.ru>
2024-05-28 13:06:56 +0300

    complete

--- practice-12.5.4_Series_of_numbers.cpp
+++ practice-12.5.4_Series_of_numbers.cpp
@@ -2,35 +2,35 @@
 
 int main() {
   std::cout << "Find the repeating number\n";
-  int numbers[15] = {5, 6, 7, 8, 14, 10, 11, 12, 13, 14, 15, 16, 17, 18, 9};
+  int numbers[15] = {5, 6, 7, 8, 14, 10, 11, 12, 13, 18, 15, 16, 17, 18, 9};
+
+  int n = std::size(numbers);
 
   int min = numbers[0];
   int sumNumbers = numbers[0];
 
-  for (int i = 1; i < 15; i++) {
+  for (int i = 1; i < n; i++) {
     if (numbers[i] < min) {
       min = numbers[i];
     }
     sumNumbers += numbers[i];
   }
 
-  int sumNormArray = min;
-  int MaxInNorm = 0;
-  for (int count = 1; count < 15; count++) {
-    sumNormArray += min + count;
-    if (count == 14) {
-      MaxInNorm = min + count;
-    }
-  }
+  int sumUnique = min * (n - 1) + ((n - 1) / 2) * (n - 2);
 
-  for (int i = 0; i < 15; i++) {
-    if ((sumNumbers - numbers[i] ==
-         sumNormArray - numbers[i] - (MaxInNorm - numbers[i])) &&
-        i != 14) {
+  int max = 0;
+  int j = 0;
+  for (int i = 0; i < n; i++) {
+    if (max < numbers[i]) {
+      max = numbers[i];
+      j = i;
+    }
+    if (sumNumbers - sumUnique == numbers[i] && numbers[i] != max ||
+        (max + numbers[i]) / 2 == max && j != i) {
       std::cout << "Repeat number: " << numbers[i] << "\n";
       break;
-    } else if (i == 14) {
-      std::cout << "No repeating numbers\n";
+    } else if (i == n - 1) {
+      std::cout << "No repeat elements\n";
     }
   }
 }
